f1e723b41b5abaf6c10de31f9075acf4ddbbb183
poholo <mailjiancheng@163.com>
2019-05-21 16:18:24 +0800

    framework

---
+++ SDK/DLNA.framework/Headers/DLNA.h
@@ -0,0 +1,25 @@
+//
+//  DLNA.h
+//  DLNA
+//
+//  Created by majiancheng on 2019/5/21.
+//  Copyright © 2019 majiancheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h"
+
+//! Project version number for DLNA.
+FOUNDATION_EXPORT double DLNAVersionNumber;
+
+//! Project version string for DLNA.
+FOUNDATION_EXPORT const unsigned char DLNAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLNA/PublicHeader.h"
+
+#import "CGUpnpControlPoint.h"
+#import "CGUpnpDevice.h"
+#import "CGUpnpService.h"
+#import "CGUpnpAction.h"
+#import "CGUpnpStateVariable.h"
+#import "CGUpnpIcon.h"
+
